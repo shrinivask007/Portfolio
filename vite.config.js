68be8f9c7b7aa46fdbc4cd863be9a4ffd1b8061e
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-06-03 09:14:10 +0530

    ver -1.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/vite-react-deploy/",
+  base : "/Protfolio/",
   css:{
     modules:{
       localsConvention: "camelCase"
