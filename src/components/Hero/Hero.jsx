cb9bec4306dedd9b6ab2c0c9fa324a759810c32a
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-06-03 10:23:43 +0530

    ver -1.0.0

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi ,I'm Shrinivas</h1>
-            <p className={styles.description}>Passionate IT Engineering student from VIIT,Pune with good programming skills and a focus on delivering exceptional results. Constantly seeking new knowledge and experiences, I stay updated with the latest trends.</p>
+            <p className={styles.description}>Passionate IT Engineering student from VIIT,Pune with good programming skills . Constantly seeking new knowledge and experiences, I stay updated with the latest trends.</p>
              <a href="mailto:shrinivassk18@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={getImageUrl("hero/myPic.png")} alt="Hero Image of me" className={styles.heroImg}/>
