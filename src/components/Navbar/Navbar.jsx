8a716839c6bafc1b21943e7c53de3dc9be3d195f
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-08-03 17:26:20 +0530

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portfolio</a>
+      <a className={styles.title} href="/Portfolio/">Portfolio</a>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
