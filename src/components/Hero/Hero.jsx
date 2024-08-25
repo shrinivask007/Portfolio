e8b6ff569618dca3d7971a8b100cce63c496faf7
Shrinivas Kathare <shrinivassk18@gmail.com>
2024-08-25 12:02:26 +0530

    Update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,7 +33,7 @@ export const Hero = () => {
        <a href="https://www.hackerrank.com/shrinivas007" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="shrinivas007" height="30" width="40" /></a>
        <a href="https://instagram.com/iamshree_007" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="iamshree_007" height="30" width="40" /></a>  
        <a href="https://twitter.com/iamshree_007" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="iamshree_007" height="30" width="40" /></a>
-       <a href="https://fb.com/hriniva.kathare.5" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="hriniva.kathare.5" height="30" width="40" /></a>
+       <a href="https://fb.com/shrinivas.kathare.007" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="hriniva.kathare.5" height="30" width="40" /></a>
        </div>
       </div>
       <img src={getImageUrl("hero/myPic.png")} alt="Hero Image of me" className={styles.heroImg} />
